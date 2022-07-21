aef8af4c7adb5e385dd5ac403014c35ac936e9a4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-21 17:59:50 +0300

    timestamp add

--- api/post/add.ts
+++ api/post/add.ts
@@ -1,7 +1,40 @@
+import { ChechAuthorization } from "../utils/token/checkAuth";
+import { connectDB } from "../utils/dbConnect/connect";
+import { Timestamp } from "../utils/timestamp";
 import { Request, Response } from "express";
 
-export const add = (req:Request, res:Response) => {
+export const add = async (req:Request, res:Response) => {
 
+    const {text} = req.body;
 
+    if(!!text){
+
+        const userId = ChechAuthorization(req, res);
+
+        try{
+    
+            if(userId){
+    
+                const connection = await connectDB();
+        
+                await connection.query("INSERT INTO todo (user_id, todo_time_start, todo_text) VALUES(?, ?, ?)", [userId, Timestamp(), text]);
+        
+                connection.end();
+            
+                res.status(201).json({message: "todo add"});
+        
+            }
+    
+        }catch(error){
+            console.log(error);
+            res.status(500).json({error: "unknown error"});
+    
+        }
+
+    }else{
+
+        res.status(403).json({error: "all fields must be filled"});
+
+    }
 
 }
---
+++ api/utils/timestamp.ts
@@ -0,0 +1,1 @@
+export const Timestamp = () => new Date().toISOString().slice(0, 19).replace('T', ' ');
